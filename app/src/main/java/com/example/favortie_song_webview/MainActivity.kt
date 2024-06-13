1abec5aaff1a9a12c209652ad65188939262ef87
kimjaesung <aamoos@naver.com>
2024-06-13 14:31:49 +0900

    웹뷰 하얀화면 대응

--- app/src/main/java/com/example/favortie_song_webview/MainActivity.kt
+++ app/src/main/java/com/example/favortie_song_webview/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.favortie_song_webview
 
  import android.os.Bundle
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Toast
-import androidx.activity.ComponentActivity
+ import android.webkit.WebResourceRequest
+ import android.webkit.WebView
+ import android.webkit.WebViewClient
+ import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
@@ -17,18 +15,15 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.webView)
-        webView.webViewClient = object : WebViewClient() {
-            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
-                super.onReceivedError(view, request, error)
-                Toast.makeText(this@MainActivity, "Failed to load webpage", Toast.LENGTH_SHORT).show()
-            }
-        }
+        webView.webViewClient = WebViewClient()
 
         webView.settings.javaScriptEnabled = true // 자바스크립트 사용을 허용
         webView.settings.useWideViewPort = true
+        webView.settings.domStorageEnabled = true
 
-        webView.loadUrl("https://favorite-song.store") // 원하는 URL을 로드
+        webView.loadUrl("https://favorite-song.store/") // 원하는 URL을 로드
     }
+
 
     override fun onBackPressed() {
         if (webView.canGoBack()) {
@@ -37,4 +32,5 @@ class MainActivity : ComponentActivity() {
             super.onBackPressed() // 기본 백버튼 동작 수행
         }
     }
+
 }
